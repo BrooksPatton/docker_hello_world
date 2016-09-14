7cd12002227c5c85c9a4e9d823dfcdb60cd3029c
Brooks Patton <brooks@brookzerker.com>
2016-09-14 12:23:24 -0600

    bringing in the config file

--- setup_project.sh
+++ setup_project.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+CONFIG_LOCATION=./config
+
 function verify_docker_is_running() {
   DOCKER_INFO_OUTPUT=$(docker info 2> /dev/null | grep "Containers:" | awk '{print $1}')
 
@@ -10,4 +14,38 @@ function verify_docker_is_running() {
   fi
 }
 
+function verify_config_exists() {
+  if [ -f $CONFIG_LOCATION ]
+  then
+    echo "config file exists!"
+  else
+    echo "Config file missing, exiting"
+    exit 2
+  fi
+}
+
+function source_config() {
+# shellcheck source=config
+# shellcheck disable=SC1091
+  source $CONFIG_LOCATION
+  EXIT_CODE=$?
+
+  check_exit_code $EXIT_CODE 'loading config file'
+}
+
+function check_exit_code() {
+  EXIT_CODE=$1
+  MESSAGE=$2
+
+  if [ "$EXIT_CODE" -ne 0 ]
+  then
+    echo "Error $MESSAGE, exiting"
+    exit "$EXIT_CODE"
+  else
+    echo "$MESSAGE"
+  fi
+}
+
 verify_docker_is_running
+verify_config_exists
+source_config
