17e83209a887efd5592ffc0a2b10d736bc61aa88
Brooks Patton <brooks@brookzerker.com>
2016-09-14 13:49:25 -0600

    creating the project directory

--- setup_project.sh
+++ setup_project.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+ERROR_DOCKER_NOT_RUNNING=1
+ERROR_CONFIG_MISSING=2
+ERROR_CHANGING_DIRECTORY=3
+ERROR_DIRECTORY_NOT_EMPTY=4
+ERROR_CONFIG_NOT_SET=5
 
 CONFIG_LOCATION=./config
 
@@ -10,7 +15,7 @@ function verify_docker_is_running() {
       echo "Docker is running, so we can continue"
     else
       echo "Docker is not running, exiting"
-      exit 1
+      exit "$ERROR_DOCKER_NOT_RUNNING"
   fi
 }
 
@@ -20,7 +25,7 @@ function verify_config_exists() {
     echo "config file exists!"
   else
     echo "Config file missing, exiting"
-    exit 2
+    exit "$ERROR_CONFIG_MISSING"
   fi
 }
 
@@ -31,6 +36,12 @@ function source_config() {
   EXIT_CODE=$?
 
   check_exit_code $EXIT_CODE 'loading config file'
+
+  if [ -z "${PROJECT_DIRECTORY}" ]
+  then
+    echo "Project directory environment variable not set, exiting"
+    exit "$ERROR_CONFIG_NOT_SET"
+  fi
 }
 
 function check_exit_code() {
@@ -46,6 +57,25 @@ function check_exit_code() {
   fi
 }
 
+function verify_project_directory() {
+  if [ -d "$PROJECT_DIRECTORY" ]
+  then
+    echo "$PROJECT_DIRECTORY exits, checking if it is empty"
+    cd "$PROJECT_DIRECTORY" || exit "$ERROR_CHANGING_DIRECTORY"
+    # shellcheck disable=SC2012
+    LINES=$(ls -l | wc -l)
+    if [ "$LINES" -ne 0 ]
+    then
+      echo "Directory is not empty, exiting"
+      exit "$ERROR_DIRECTORY_NOT_EMPTY"
+    fi
+  else
+    echo "Creating $PROJECT_DIRECTORY"
+    mkdir "$PROJECT_DIRECTORY"
+  fi
+}
+
 verify_docker_is_running
 verify_config_exists
 source_config
+verify_project_directory
